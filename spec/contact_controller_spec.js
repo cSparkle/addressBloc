7534ff4d275952a27ec4ac96ab4542ebb0160134
Cortney Thomas <thomas.cortney@outlook.com>
2018-11-19 18:20:57 -0500

    Assignment add email: completed

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -15,9 +15,10 @@ describe('ContactController', () => {
 
     describe('#addContact()', () => {
         it('should add a single contact into the book', (done) => {
-            this.book.addContact('Alice', '001-101-1010').then((contact) => {
+            this.book.addContact('Alice', '001-101-1010', 'alice@email.com').then((contact) => {
                 expect(contact.name).toBe('Alice');
                 expect(contact.phone).toBe('001-101-1010');
+                expect(contact.email).toBe('alice@email.com');
                 done();
             }).catch((err) => {
                 done();
